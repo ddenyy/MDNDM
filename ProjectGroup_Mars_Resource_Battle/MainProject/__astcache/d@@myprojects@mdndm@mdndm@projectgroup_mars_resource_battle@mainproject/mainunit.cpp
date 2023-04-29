54201b4b7c4f92b0b3980d7dd3f855183e3979c0
Chirkov <mlhailchirk@gmail.com>
2023-04-29 22:19:27 +0300

    сделал интерейс артефактов

--- ProjectGroup_Mars_Resource_Battle/MainProject/__astcache/d@@myprojects@mdndm@mdndm@projectgroup_mars_resource_battle@mainproject/mainunit.cpp
+++ ProjectGroup_Mars_Resource_Battle/MainProject/__astcache/d@@myprojects@mdndm@mdndm@projectgroup_mars_resource_battle@mainproject/mainunit.cpp
@@ -30,26 +30,27 @@ public:
         sphere -> Height = 1;
         sphere -> Width = 1;
         sphere -> Depth = 1;
-        sphere -> Position -> Y -= (0.05 + root_cube -> Height * 0.5);
+        sphere -> Position -> Y -= (0.05 + root_cube -> Height * 0.5 + 0.5);
+        material = new TLightMaterialSource(root_cube);
 
         if (artefact_type == "rare") {
             sphere -> Width = 0.8;
 	        sphere -> Depth = 0.8;
-            material -> Diffuse = claTurquoise;
+            material -> Diffuse = 0x00ff0000;
             sphere -> MaterialSource = material;
         }
 
         if (artefact_type == "usually") {
             sphere -> Width = 0.5;
 	        sphere -> Depth = 0.5;
-            material -> Diffuse = claIndianred;
+            material -> Diffuse = 0x000000ff;
             sphere -> MaterialSource = material;
         }
 
         if (artefact_type == "frequent") {
             sphere -> Width = 0.3;
 	        sphere -> Depth = 0.3;
-            material -> Diffuse = claMediumspringgreen;
+            material -> Diffuse = 0x0000ff00;
             sphere -> MaterialSource = material;
         }
     }
@@ -213,7 +214,7 @@ void __fastcall TMainForm::GameRectButClick(TObject *Sender)
     //turn on game page
     MainTabControl -> ActiveTab = GameTab;
 
-    int board_height = 20, board_width = 20;
+    int board_height = 15, board_width = 15;
 
     //интерфейс поля
     vector <vector <InterfaceCell> > IBoard(board_height, vector <InterfaceCell>(board_width));
